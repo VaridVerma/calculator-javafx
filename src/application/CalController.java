9c019fdf617349b3a1d400207648437c4c8a5530
VaridVerma <varid.verma@gmail.com>
2018-01-28 01:59:12 +0530

    first commit

---
+++ src/application/CalController.java
@@ -0,0 +1,52 @@
+package application;
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CalController {
+	
+	@FXML
+	private Label result;
+	private long n1 = 0;
+	private String operator;
+	private boolean start = true;
+	private Model model = new Model();
+	
+	@FXML
+	public void processNumbers(ActionEvent event){
+		if(start){
+			result.setText("");
+			start = false;
+		}
+		String value = ((Button)event.getSource()).getText();
+		result.setText(result.getText()+value);
+	}
+	
+	@FXML
+	public void processOperator(ActionEvent event){
+		String value = ((Button)event.getSource()).getText();
+		
+		if(!operator.equals("=")){
+			if(!operator.isEmpty())
+				return;
+			
+			operator = value;
+			n1 = Long.parseLong(result.getText());
+			result.setText("");
+		}else{
+			if(operator.isEmpty())
+				return;
+			long n2 = Long.parseLong(result.getText());
+			float output = model.calculate(n1, n2, operator);
+			result.setText(String.valueOf(output));
+			operator = "";
+			start = true;
+				
+			}
+		}
+	}
+
---
+++ src/application/Model.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Model {
+	public float calculate(long n1, long n2, String operator) {
+		switch(operator){
+			case "+":
+				return n1 + n2;
+			case "-":
+				return n1 - n2;
+			case "*":
+				return n1 * n2;
+			case "/":
+				if(n2 == 0)
+					return 0;
+				return n1 / n2;
+				
+			default:
+				return 0;
+		}
+	}
+}
